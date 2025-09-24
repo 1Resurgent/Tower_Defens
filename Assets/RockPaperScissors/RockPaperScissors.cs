919a487bd4d667baf36f8c357e478102e6cc9c3b
Resurgent <Aleksandr.Skirda@gmail.com>
2025-09-24 18:48:25 +0300

    Камень-ножницы-Бумага 1

--- Assets/RockPaperScissors/RockPaperScissors.cs
+++ Assets/RockPaperScissors/RockPaperScissors.cs
@@ -5,35 +5,79 @@ public class RockPaperScissors : MonoBehaviour
 {
     [SerializeField] private int Points;
     [SerializeField] private int AiPoints;
+    [SerializeField] private float Timer;
+
+    [SerializeField] private GameObject StartButton, RockButton, PaperButton, ScissorsButton;
+
     [SerializeField] private TextMeshProUGUI PointsText;
-    
     [SerializeField] private TextMeshProUGUI AiPointsText;
+    [SerializeField] private TextMeshProUGUI TimerText;
+    [SerializeField] private GameObject TimerGM;
 
     private Tools selectedThing;
     private Tools selectedByAI;
 
+    public enum Tools
+    {
+        Rock = 0,
+        Paper = 1,
+        Scissors = 2
+    }
+
+    public void Update()
+    {
+        Timer -= Time.deltaTime;
+        TimerText.text = "Время : " + Timer.ToString("00");
+
+        if (Timer <= 0)
+        {
+            TimerGM.SetActive(false);
+        }
+    }
+
+
     public void AddOnePoint()
     {
-        Points = Points + 1;
-        PointsText.text = "Очки: " + Points;
+        Points += 1;
+        PointsText.text = "Player : " + Points;
     }
+
+    public void AddAIOnePoint()
+    {
+        AiPoints += 1;
+        AiPointsText.text = "AI : " + Points;
+    }
+
+
+    public void StartButtom()
+    {
+        StartButton.SetActive(false);
+        RockButton.SetActive(true);
+        PaperButton.SetActive(true);
+        ScissorsButton.SetActive(true);
+        TimerGM.SetActive(true);
+        Timer = 15f;
+    }
+
+
+
+
+
+
+
 
     public void SelectRock()
     {
         selectedThing = Tools.Rock;
+
         selectedByAI = (Tools)Random.Range(0, 3);
         if (selectedByAI == Tools.Paper)
         {
-            AiPoints += 1;
-            AiPointsText.text = "Очки ИИ: " + AiPoints;
+            AddAIOnePoint();
         }
         else if (selectedByAI == Tools.Scissors)
         {
             AddOnePoint();
-        }
-        else if (selectedByAI == Tools.Rock)
-        {
-            
         }
     }
     
@@ -44,16 +88,11 @@ public class RockPaperScissors : MonoBehaviour
         selectedByAI = (Tools)Random.Range(0, 3);
         if (selectedByAI == Tools.Scissors)
         {
-            AiPoints += 1;
-            AiPointsText.text = "Очки ИИ: " + AiPoints;
+            AddAIOnePoint();
         }
         else if (selectedByAI == Tools.Rock)
         {
             AddOnePoint();
-        }
-        else if (selectedByAI == Tools.Paper)
-        {
-            
         }
     }
     
@@ -63,23 +102,12 @@ public class RockPaperScissors : MonoBehaviour
         selectedByAI = (Tools)Random.Range(0, 3);
         if (selectedByAI == 0)
         {
-            AiPoints += 1;
-            AiPointsText.text = "Очки ИИ: " + AiPoints;
+            AddAIOnePoint();
         }
         else if (selectedByAI == Tools.Paper)
         {
             AddOnePoint();
         }
-        else if (selectedByAI == Tools.Scissors)
-        {
-            
-        }
     }
 
-    public enum Tools
-    {
-        Rock = 0,
-        Paper = 1,
-        Scissors = 2
-    }
 }
